87e6a89d045a9d34e40c6a2f48d043e9bc9472cf
Colin Miao <sddzok@163.com>
2017-07-27 09:01:10 +0800

    change file

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("h6,.index-tips-arrow").on("click",function(){
-        $("body").animate({opacity:"0"},1000, function(){      
-            location.href="./framework/index.html";        
-        })
-    });
-});
