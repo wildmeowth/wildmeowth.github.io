8eea3364b45809430e4fe1d9a95a6a0b6ad080c4
WildMeowthUbuntu <sddzok@163.com>
2022-05-30 14:53:37 +0800

    add baidu site verification

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ $(document).ready(function() {
   var collapsedPanel = function() {
     currentWidth = $('.panel-cover').width();
     var bgImage = $('.panel-cover').css('background-image');
-    var adjustHeight = currentWidth < 991 ? 'auto' : '40%';
+    var adjustHeight = currentWidth < 991 ? currentWidth < 486 ? '229' : '242' : '40%';
 
     $('.panel-cover').removeAttr('style');
     $('.panel-cover').css('background-image', bgImage);
